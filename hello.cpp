1d15418f34738cc91cdd02968ddebeb9e2a6ad1a
anishbelel <anishbelel2212@gmail.com>
2025-06-28 18:54:19 +0530

    Day 3: Added Day 3 message

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "This is Day 2 update." << endl;
+    cout << "Welcome to Day 3!" << endl;
     return 0;
 }
---
+++ hi.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; //*a.find_by_order(x) a.order_of_key(x) a.insert(x) a.erase(x) 
+// typedef tree<int, null_type, greater<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set1;
+
+#define int long long
+#define fr(i,n) for(int i=0;i<n;i++)
+#define frr(i,n) for(int i=n;i>=0;i--)
+typedef pair<int,int>pr;
+typedef vector<int>vii;
+typedef vector<pr>vip;
+typedef map<int,int> maa;
+#define co cout <<
+#define en << endl
+#define sor(vec) sort(vec.begin(),vec.end())
+#define rsor(vec) sort(vec.begin(),vec.end(),greater<int>())
+#define upp(vec,idx,x) upper_bound(vec.begin()+idx,vec.end(),x)-vec.begin()
+
+int n, k;
+
+void solve(){
+
+    cin >> n >> k;
+    vector<int> vec(n);
+    for(int i = 0; i < n; i++) cin >> vec[i];
+    
+}
+
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+    int t;cin >> t;while(t--)
+    solve();
+}
